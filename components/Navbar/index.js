fb5f7c8fef353bec8e721fde9428ed06f50f738b
burak subasi <burak.subasi@patikaglobal.com>
2023-08-31 17:44:18 +0300

    style update

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -8,42 +8,59 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link';
 import { useFirebaseAuthentication } from '../../utils/firebase';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
-
-export default function Navbar({setIsSideBarOpen}) {
+export default function Navbar({ setIsSideBarOpen }) {
   const navList = [
-    {name:"ANASAYFA",link:"/"},
-    {name:"HAKKIMIZDA",link:"about"},
-    {name:"HİZMETLERİMİZ",link:"services"},
-    {name:"MEDYA",link:"media"},
-    {name:"İLETİŞİM",link:"communication"},
-    {name:"GÖRÜŞ VE ÖNERİ",link:"suggestionandopinion"}]
+    { name: "ANASAYFA", link: "/" },
+    { name: "HAKKIMIZDA", link: "about" },
+    { name: "HİZMETLERİMİZ", link: "services" },
+    { name: "MEDYA", link: "media" },
+    { name: "İLETİŞİM", link: "communication" },
+    { name: "GÖRÜŞ VE ÖNERİ", link: "suggestionandopinion" }]
 
   const authUser = useFirebaseAuthentication();
+  const isMobile  = useMediaQuery('(max-width:800px)');
   return (
-    <Box sx={{ flexGrow: 1, margin:"0 px",width:"100%" }}>
-      <AppBar className='nav-bar'  position="static" >
+    <Box sx={{ flexGrow: 1, margin: "0 px", width: "100%" }}>
+      <AppBar className='nav-bar' position="static" >
         <Toolbar>
-            <IconButton
+          {isMobile && <IconButton
             size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
-            onClick={()=>setIsSideBarOpen(true)}
+            onClick={() => setIsSideBarOpen(true)}
           >
             <MenuIcon />
-            </IconButton>
-            {navList.map((item,index)=>{
-              return(
+          </IconButton>}
+          <div>
+
+            {!isMobile && navList.slice(0, 3).map((item, index) => {
+              return (
                 <Link key={index} href={item.link}  >
                   <Button key={index} color="inherit">{item.name} </Button>
                 </Link>
               )
             })}
-          {authUser ?(<Link href="/dashboard"  >
-                  <Button  color="inherit">Dashboard </Button>
-                </Link>):null }
+          </div>
+          <div>
+           <Button key={1} color="inherit"> ORAL SAĞLIK </Button>
+          </div>
+          <div>
+
+            {!isMobile && navList.slice(3).map((item, index) => {
+              return (
+                <Link key={index} href={item.link}  >
+                  <Button key={index} color="inherit">{item.name} </Button>
+                </Link>
+              )
+            })}
+          </div>
+          {authUser ? (<Link href="/dashboard"  >
+            <Button color="inherit">Dashboard </Button>
+          </Link>) : null}
         </Toolbar>
       </AppBar>
     </Box>
--- pages/suggestionandopinion/index.js
+++ pages/suggestionandopinion/index.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import Layout from '../../components/Layout'
-import  FormInput from "../../components/FormInput"
+import  SuggestionForm from "../../components/SuggestionForm"
 import CustomSnackbar from '../../components/CustomSnackbar'
-import { Button } from '@mui/material'
+import { Button, Typography } from '@mui/material'
 const SuggestionandOpinion = () => {
   const [open, setOpen] = useState(false);
   const [severity, setSeverity] = useState("");
@@ -15,11 +15,10 @@ const SuggestionandOpinion = () => {
   }
   return (
     <Layout>
-        <div>
+        
           <CustomSnackbar handleClose={handleClose} open={open} severity={severity} text={text}/>
-          <FormInput  handleClose={handleClose} handleOpen={handleOpen} setSeverity={setSeverity} setText={setText} />
-          <Button onClick={handleOpen} >aç</Button>
-        </div>
+          <SuggestionForm  handleClose={handleClose} handleOpen={handleOpen} setSeverity={setSeverity} setText={setText} />
+     
     </Layout>
   )
 }
